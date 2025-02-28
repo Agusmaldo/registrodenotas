22e3787733f5695b53a0a52826cac80e57bfb293
Agustín Ignacio Maldonado <you@example.comz>
2025-02-28 13:19:49 -0300

    cambio de nom

--- plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/signinSuccess.php
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,7 +1,7 @@
 <div style='text-align: center'>
     <?php use_helper('I18N') ?>
 
-    <h1><?php echo __('SEGUIMIENTO EXPEDIENTES FERROVIARIOS', null, 'sf_guard') ?></h1>
+    <h1><?php echo __('REGISTRO DE NOTAS', null, 'sf_guard') ?></h1>
 
 <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
 </div>
